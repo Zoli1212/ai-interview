5340c199588928ced375d59cefecc2ec0fa997a3
zoltan-molnar <rdigital4455@gmail.com>
2025-12-25 22:07:11 +0100

    fix06

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,10 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
     return (
         <div className='flex items-center justify-center h-screen'>
-            <SignIn />
+            <SignIn
+                forceRedirectUrl="/dashboard"
+                signUpUrl="/sign-up"
+            />
         </div>
     )
 }
