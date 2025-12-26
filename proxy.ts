af20bf2a34850ba99e381232b061a66849ae1131
zoltan-molnar <rdigital4455@gmail.com>
2025-12-26 20:19:36 +0100

    clerk

--- proxy.ts
+++ proxy.ts
@@ -7,10 +7,10 @@ const isPublicRoute = createRouteMatcher([
     '/learning/(.*)/start(.*)'
 ])
 
-export default clerkMiddleware(async (auth, req) => {
-    if (!isPublicRoute(req)) {
-        await auth.protect()
-    }
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
 })
 
 export const config = {
